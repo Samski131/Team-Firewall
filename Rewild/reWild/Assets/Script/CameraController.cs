cccb49745a6856891cfb72e75c093a61d58b43e6
Adam Campbell <acampbell97@hotmail.co.uk>
2017-10-05 17:33:56 +0100

    Added Protoype
    Uploaded the finished prototype that was worked on on 05/10/17
    
    -Added Camera Switching
    -Added Smooth Camera Script
    -Added Wolf Model
    -Added Music (Looping) (Temp)
    -Added Terrain and Foilage
    -Adjusted Collision Script

---
+++ Rewild/reWild/Assets/Script/CameraController.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public bool isThirdperson = true;
+    public bool isTranslating = false;
+    public float cameraPanSpeed;
+    public GameObject FirstPersonCameraAnchor;
+    private Camera FirstPersonCamera; //enables the First Person Camera.;
+                                      // Use this for initialization
+    public GameObject Human;
+    public GameObject Wolf;
+ 
+
+
+    void Start()
+    {
+        FirstPersonCamera = FirstPersonCameraAnchor.transform.Find("FPSCamera").GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            Debug.Log("C Key Press");
+        }
+
+            if (Input.GetKeyDown(KeyCode.V))
+        {
+            Debug.Log("v Key Press");
+            if (isThirdperson == true)
+            { // switching from third person to first person
+                Debug.Log("Entering 1st Person");
+                isThirdperson = false; // switches to first person
+
+                isTranslating = true; // starts translating
+            }
+            else if (isThirdperson == false)
+            {
+                Debug.Log("Entering 3rd Person");
+                isThirdperson = true; // switches to third person
+
+                isTranslating = true; // starts translating
+            }
+
+
+        }
+
+
+        if ((isTranslating) && (isThirdperson == false)) //if the camera is translating TO first person
+        {
+            Debug.Log("Translating to 1st Person");
+            // Debug.Log("Distance: " + Vector3.Distance(transform.position, FirstPersonCameraAnchor.transform.position));
+            transform.position = Vector3.MoveTowards(transform.position, FirstPersonCameraAnchor.transform.position, (cameraPanSpeed * Time.deltaTime)); //move the camera forwards smoothly based on public speed variable
+
+            if (Vector3.Distance(transform.position, FirstPersonCameraAnchor.transform.position) < 0.1)
+            { //if the Third person camera is roughly where it needs to be
+                isTranslating = false;
+                transform.localPosition = new Vector3(0, 0, 0); //resets the thirdperson camera back to the anchor location for next switch.
+                gameObject.GetComponent < Camera >().enabled = false; //disables Third person camera
+                FirstPersonCameraAnchor.transform.Find("FPSCamera").gameObject.SetActive(true); //enables the First Person Camera.
+                Wolf.GetComponent<MeshRenderer>().enabled = true;
+                Human.GetComponent<SkinnedMeshRenderer>().enabled = false;
+                gameObject.transform.parent.transform.parent.GetComponent<CapsuleCollider>().enabled = false;
+                Wolf.GetComponent<CapsuleCollider>().enabled = true;
+            }
+
+        }
+        else if ((isTranslating) && (isThirdperson == true)) // if camera is translating TO third person
+        {
+            Debug.Log("Translating to 3rd Person");
+            //Debug.Log("Distance: " + Vector3.Distance(transform.position, FirstPersonCameraAnchor.transform.position));
+            FirstPersonCamera.transform.position = Vector3.MoveTowards(FirstPersonCamera.transform.position, transform.parent.position, (cameraPanSpeed * Time.deltaTime)); //move the camera forwards smoothly based on public speed variable
+
+            if (Vector3.Distance(FirstPersonCamera.transform.position, transform.parent.position) < 0.1)
+            { //if the First person camera is roughly where it needs to be
+                isTranslating = false;
+                FirstPersonCamera.transform.gameObject.SetActive(false); //disables the first person camera.
+                gameObject.GetComponent<Camera>().enabled = true; //disables Third person camera
+                FirstPersonCamera.transform.localPosition = new Vector3(0, 0, 0);
+                Wolf.GetComponent<MeshRenderer>().enabled = false;
+                Human.GetComponent<SkinnedMeshRenderer>().enabled = true;
+                gameObject.transform.parent.transform.parent.GetComponent<CapsuleCollider>().enabled = true;
+               Wolf.GetComponent<CapsuleCollider>().enabled = false;
+            }
+        }
+    }
+
+}
---
+++ Rewild/reWild/Assets/Script/CameraSwitch.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSwitch: MonoBehaviour
+{
+
+
+    public Camera[] cams;
+    public SkinnedMeshRenderer Human;
+    public MeshRenderer Wolf;
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            //If FPS Camera is on, switch to 3rd person
+            if (Wolf.enabled == true)
+            {
+                Human.enabled = true;
+                Wolf.enabled = false;
+
+            }
+            else
+            {
+                Wolf.enabled = true;
+                Human.enabled = false;
+            }
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            //If FPS Camera is on, switch to 3rd person
+            if (cams[0].enabled == true)
+            {
+                cams[1].enabled = true;
+                cams[0].enabled = false;
+
+            }
+            //If 3rd Person Camera is on, switch to FPS
+            else
+            {
+                cams[1].enabled = false;
+                cams[0].enabled = true;
+
+            }
+
+        }
+
+    }
+}
