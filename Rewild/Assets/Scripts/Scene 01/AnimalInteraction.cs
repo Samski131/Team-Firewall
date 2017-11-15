61248fb79da08eff354679a74e118b4ff87d0a11
Panagiotis-Katsiadramis <32579858+Panagiotis-Katsiadramis@users.noreply.github.com>
2017-11-15 21:49:50 +0000

    Added a basic interaction element between the player and the animal.
    When the player enters the area of the animal, if we press the key H the interaction will be increased , but still it will decrease slowly until the maximum interaction is achieved.
    If the players moves to far away of the animal the interaction will fall very fast.

---
+++ Rewild/Assets/Scripts/Scene 01/AnimalInteraction.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AnimalInteraction : MonoBehaviour {
+    private bool didThePlayerEnter; // did the player enter the collision area
+    public Scrollbar interaction;
+    private bool interactionAchieved;
+
+	// Use this for initialization
+	void Start () {
+        didThePlayerEnter = false;
+        //interaction.value = 0.5f;
+        interactionAchieved = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (interaction.value >0.98f)// 
+        {
+            interactionAchieved = true;
+        }
+       if (interaction.value<0.1)
+        {
+            interactionAchieved = false;
+        }
+
+
+        if (didThePlayerEnter)
+        {
+            if (!interactionAchieved) // if its achieved the player doesnt need to do something else. 
+            {
+                interaction.value -= 0.01f; // the animal loses trus in a smaller degree when the player is close
+                if (Input.GetKeyDown(KeyCode.H))
+                {
+                    interaction.value += 0.1f;
+
+                }
+            }
+        }
+        else
+        {
+           interaction.value -= 0.1f;
+        }
+	}
+
+    void OnTriggerEnter(Collider collider)
+    {
+        Debug.Log("colision entered");
+        didThePlayerEnter = true;
+    }
+
+     void OnTriggerExit(Collider collider)
+    {
+        Debug.Log("colision stoped");
+        didThePlayerEnter = false;
+    }
+}
+//Made by Panagiotis Katsiadramis
+//Modified by:
