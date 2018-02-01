9840c0ad09898742bbdf7e4c97c4aca26611f148
Panagiotis-Katsiadramis <32579858+Panagiotis-Katsiadramis@users.noreply.github.com>
2018-02-01 02:15:09 +0000

    ambient sound implementation
    implemented a 3d sound system for the ambient sound of the game with the use of unity's audio mixer in our demo scene .

---
+++ Rewild/Assets/Scripts/Scene 01/AmbientSound.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AmbientSound : MonoBehaviour {
+
+
+    public AudioMixerSnapshot insideTheForest;
+    public AudioMixerSnapshot OutsideTheForest;
+    public float transitionTime;
+
+    //// Use this for initialization
+    //void Start () {
+
+    //}
+
+    //// Update is called once per frame
+    //void Update () {
+
+    //}
+
+     void OnTriggerEnter(Collider other)
+    {
+        insideTheForest.TransitionTo(transitionTime);
+    }
+
+     void OnTriggerExit(Collider other)
+    {
+        OutsideTheForest.TransitionTo(transitionTime);   
+    }
+}
