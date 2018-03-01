b51d2114e83c17cf6c72467b811578ccff491871
Samski131 <samfgray@me.com>
2018-03-01 17:33:03 +0000

    Updated Navmesh, added fox interaction
    -Updated Navmesh script
    -Only navmesh is implemented
    -Made coliders for Fox bigger
    -NO animations or sounds implemented

--- Rewild/Assets/Scripts/Scene 01/NavMeshTest.cs
+++ Rewild/Assets/Scripts/Scene 01/NavMeshTest.cs
@@ -20,16 +20,28 @@ public class NavMeshTest : MonoBehaviour
 
     //Public variable to track the foxes movement
 
+	public enum STATE
+	{
+		FLEEING,
+		IDLE,
+		CURIOUS,
+		EATING
+	}
+
     Transform AiDestination;
     public Vector3 target;
     public bool moving = false;
 	public int state = 1;
     public bool triggered;
-    public int framesSinceTriggered;
+	public int framesSinceTriggered;
+	public Vector3 lastPosition;
+	private BoxCollider colldier;
+	private GameObject food;
+	public STATE currentState;
 
-    public Vector3 lastPosition;
+	public bool flag = true;
 
-    private BoxCollider colldier;
+
 	void Start ()
     {
         //Initiliase the variables
@@ -42,6 +54,8 @@ public class NavMeshTest : MonoBehaviour
         SetDestination(AiDestination, 5);
         state = 2;
 
+		currentState = STATE.IDLE;
+
         //Debugging
         if (navMeshAgent == null)
         {
@@ -50,15 +64,25 @@ public class NavMeshTest : MonoBehaviour
 		
 	}
 
-    void OnTriggerEnter()
+	void OnTriggerEnter(Collider other)
     {
-        triggered = true;
+		if(other.tag == "Player")
+		{
+			Debug.Log("Moving to Running State");
+			currentState = STATE.FLEEING;
+		}
+		else if ((currentState == STATE.IDLE) && (other.tag == "Food"))
+		{
+			Debug.Log("Moving to CURIOUS State");
+			currentState = STATE.CURIOUS;
+			food = GameObject.FindGameObjectWithTag("Food");
+		}
+
     }
 
-    void OnTriggerExit()
+	void OnTriggerExit(Collider other)
     {
         framesSinceTriggered = 0;
-        triggered = false;
     }
 
     void Update()
@@ -77,58 +101,135 @@ public class NavMeshTest : MonoBehaviour
             framesSinceTriggered = 0;
         }
 
-        //Switch statement to ove the fox when triggered by player
-        if (triggered && !moving)
-        {
 
-            //Move Nav onto the next waypoint
-            switch(state)
-            {
-                case 1:
-                    AiDestination = destination;
-                    state = 2;
-                    break;
-                case 2:
-                    AiDestination = destination2;
-                    state = 3;
-                    break;
-                case 3:
-                    AiDestination = destination3;
-                    state = 4;
-                    break;
-                case 4:
-                    AiDestination = destination4;
-                    state = 1;
-                    break;
+		//MASTER STATE MACHINE
 
-            }
+		switch(currentState)
+		{
 
-            //Set the destination and remove the flag to enter this statement
-            SetDestination(AiDestination, 5);
-            triggered = false;
+			case STATE.CURIOUS:
+				curiousState();
+				break;
 
-        }
-        else if(!moving && (framesSinceTriggered > 150))
+			case STATE.EATING:
+				eatingState();
+				break;
 
+			case STATE.FLEEING:
+				runningState();
+				break;
 
+			case STATE.IDLE:
+				wanderState();
+				break;
+		}
 
-        {       
-            //Move to a random destinatinon
-
-            Transform wanderDestination = AiDestination;
-
-            Vector3 rand = UnityEngine.Random.insideUnitSphere;
-            wanderDestination.position = wanderDestination.position + (rand*6);
-			SetDestination(wanderDestination, 6);
-            framesSinceTriggered = 0;
-            
-        }
         
     }
+
+	private void curiousState()
+	{
+		Transform target;
+		target = food.transform;
+
+		SetDestination(target, 2);
+
+		if(checkDestination(target.position))
+			{
+				//eat the food
+				currentState = STATE.FLEEING;
+				DestroyObject(food);
+			}
+		//Get the location of the food
+		//Check that the player isnt around
+		//Move the fox slowly towards the food
+	}
+
+	private void eatingState()
+	{
+		//Debug.Log("This is Eating State");
+
+	}
+
+	private void runningState()
+	{
+
+		//Debug.Log("This is Running State");
+
+
+		if(!moving && flag)
+		{
+			//Move Nav onto the next waypoint
+			switch(state)
+			{
+			case 1:
+				AiDestination = destination;
+				state = 2;
+				break;
+			case 2:
+				AiDestination = destination2;
+				state = 3;
+				break;
+			case 3:
+				AiDestination = destination3;
+				state = 4;
+				break;
+			case 4:
+				AiDestination = destination4;
+				state = 1;
+				break;
+			}
+
+			//Set the destination and remove the flag to enter this statement
+			SetDestination(AiDestination, 7);
+			flag = false;
+		}
+		else
+		{
+			if(checkDestination(AiDestination.position) == true)
+			{
+				currentState = STATE.IDLE;
+				framesSinceTriggered = 0;
+				flag = true;
+			}
+		}
+
+
+	}
+
+	private bool checkDestination(Vector3 destination)
+	{
+		if(Mathf.Abs(this.transform.position.magnitude - destination.magnitude) < 1.0f)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	private void wanderState()
+	{
+		//Debug.Log("This is Wander State");
+
+		//Move to a random destinatinon
+		if(!moving && (framesSinceTriggered > 150))
+		{       
+			Transform wanderDestination = AiDestination;
+
+			Vector3 rand = UnityEngine.Random.insideUnitSphere;
+			wanderDestination.position = wanderDestination.position + (rand*6);
+			SetDestination(wanderDestination, 6);
+			framesSinceTriggered = 0;
+		}
+
+	
+	}
+
 	
     private void SetDestination(Transform destination, int speed)
     {
-
         //Set the new destination in the NavMeshAgent
         if(destination !=null)
         {
@@ -138,6 +239,4 @@ public class NavMeshTest : MonoBehaviour
 			target = targetVector;
         }
     }
-
-
 }
