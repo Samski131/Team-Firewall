61315c640ca2326b9c06f66021925678c07533e0
Sam Gray <samfgray@me.com>
2018-04-13 19:28:29 +0100

    Added tracking mechanic and ragdoll dead bird
    -Added a script to create footprints behind an actor.
    
    -Added physics to bird model.

---
+++ Rewild/Assets/Scripts/Scene 01/scr_Footprints.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_Footprints : MonoBehaviour {
+
+    public bool leavePrints;
+    public bool isFox; //for determining which kind of prints to leave as Mollie changes hers.
+    public GameObject foxPrints;
+    public GameObject bootPrints;
+    private Vector2 lastFootprint;
+    private Vector2 currentPosition;
+    private Terrain terrain;
+    public Queue<GameObject> printQueue;
+    public Color trailColor;
+    public float StrideDistance; //how far between each step
+    public int TrailLength; // how long a trail can be before steps begin getting deleted
+    // Use this for initialization
+	void Start () 
+    {
+      terrain = GameObject.FindGameObjectWithTag("Terrain").GetComponent<Terrain>();
+      lastFootprint = new Vector2(this.transform.position.x, this.transform.position.z); // just to get a 2d representation
+     printQueue = new Queue<GameObject>();
+        TrailLength++; // as counting from 0 makes the length slightly shorter than expected.
+	}
+	
+
+    float distanceMoved ()
+    {
+        return (Vector2.Distance(lastFootprint, currentPosition));        
+    }
+
+	// Update is called once per frame
+	void Update () 
+    {
+        currentPosition = new Vector2(this.transform.position.x, this.transform.position.z); //we don't care about Y values when working out distance
+        if (distanceMoved() >= StrideDistance)
+            {
+            
+            if (isFox)
+            {
+                GameObject temp = GameObject.Instantiate(foxPrints, new Vector3(currentPosition.x, terrain.SampleHeight(this.transform.position)+ 0.01f, currentPosition.y), this.transform.localRotation);
+                temp.GetComponent<Renderer>().material.SetColor("_EmissionColor", trailColor);
+                RaycastHit hit;
+                lastFootprint.Set(currentPosition.x, currentPosition.y);
+                if (Physics.Raycast(this.transform.position, transform.TransformDirection(Vector3.down), out hit, Mathf.Infinity))
+                {
+                    temp.transform.rotation = Quaternion.FromToRotation(transform.up, hit.normal) * temp.transform.rotation;
+                    printQueue.Enqueue(temp); //current pos.y is actually Z position
+                }   
+            }
+            else
+            {
+                GameObject temp = GameObject.Instantiate(bootPrints, new Vector3(currentPosition.x, terrain.SampleHeight(this.transform.position) + 0.01f, currentPosition.y), this.transform.localRotation);
+                temp.GetComponent<Renderer>().material.SetColor("_EmissionColor", trailColor);
+                RaycastHit hit;
+                lastFootprint.Set(currentPosition.x, currentPosition.y);
+                if (Physics.Raycast(this.transform.position, transform.TransformDirection(Vector3.down), out hit, Mathf.Infinity))
+                {
+                    temp.transform.rotation = Quaternion.FromToRotation(transform.up, hit.normal) * temp.transform.rotation;
+                    printQueue.Enqueue(temp); //current pos.y is actually Z position
+                }   
+            }
+        }
+            
+        if (printQueue.Count >= TrailLength)
+        {
+            GameObject.Destroy(printQueue.Peek());
+            printQueue.Dequeue();
+        }
+    }
+}
+        		
+	
+
