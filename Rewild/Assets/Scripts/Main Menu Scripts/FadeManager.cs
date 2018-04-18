7dd700502b13ba0b9b27605ec5f4ce41d915ef05
Panagiotis-Katsiadramis <32579858+Panagiotis-Katsiadramis@users.noreply.github.com>
2018-04-18 22:51:56 +0100

    Finished the demo of the new menu.
    The mechanic works  fine with the keyboard , needs to be maped with the vr controlers , artist and the designer must decide which scene they will use for the new menu to import everything on the final menu scene.

--- Rewild/Assets/Scripts/Main Menu Scripts/FadeManager.cs
+++ Rewild/Assets/Scripts/Main Menu Scripts/FadeManager.cs
@@ -7,15 +7,12 @@ using UnityEngine.SceneManagement;
 public class FadeManager : MonoBehaviour
 {    
     private Image loadingScreen;
-    private GameObject myCamera;
+   
     private float transaction;
     private bool startFadeIn;
     private bool startFadeOut;
     private bool executeTheIfStatement;
-    private Vector3 cameraPosition;
-    private Vector3 requiredPosition; // The position that when the camera will reach , will trigger the fade in function.
-    private float zDifference;
-    public bool isTheCameraMoving { get; set; } // this bool is set to true in the MainMenu script , in order not to consume cpu resources without reason on math operations.
+   
     private Scene currentScene; // used to check which scene is active
     private string sceneName;
     public float fadeTime;
@@ -24,16 +21,10 @@ public class FadeManager : MonoBehaviour
     // Use this for initialization
     void Start ()
     {
-        isTheCameraMoving = false;
         startFadeIn = false;
-        requiredPosition = new Vector3(653.0f, 105.0f, 592.0f);  // on this coordinates the Z compoment is the one that interests us
-        myCamera = GameObject.Find("Camera");
-        cameraPosition = myCamera.transform.position;
-        zDifference = 1.0f;
         transaction = 0.0f;
         executeTheIfStatement = false;
         startFadeOut = false;
-
         loadingScreen = GameObject.Find("Fade/Canvas").GetComponentInChildren<Image>();
         Debug.Log("01");
     }
@@ -41,18 +32,7 @@ public class FadeManager : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
     {
-        if (isTheCameraMoving)  
-        {
-            Debug.Log("02");
-            cameraPosition = myCamera.transform.position;
-            if((cameraPosition.z - requiredPosition.z) >= zDifference)
-            {
-                Debug.Log("03");
-                startFadeIn = true;
-                isTheCameraMoving = false;
-            }
-        }
-
+      
       if(startFadeIn)
         {
             Debug.Log("04");
@@ -99,8 +79,6 @@ public class FadeManager : MonoBehaviour
                 //Destroy(loadingScreen.transform.parent);
                 Destroy(gameObject);
             }
-
-
         }
     }
 
@@ -112,6 +90,11 @@ public class FadeManager : MonoBehaviour
     private void fadeOut()
     {
         loadingScreen.color = Color.Lerp(Color.white, new Color(1.0f, 1.0f, 1.0f, 0.0f), transaction);
+    }
+
+    public void SetFadeInTrue()
+    {
+        startFadeIn = true;
     }
 }
 //Made by Panagiotis Katsiadramis 
--- Rewild/Assets/Scripts/Main Menu Scripts/MainMenu.cs
+++ Rewild/Assets/Scripts/Main Menu Scripts/MainMenu.cs
@@ -5,131 +5,58 @@ using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
-    public Animator animator;
-    public AudioSource audioEffects;
+   
+   // public AudioSource audioEffects;
     private AudioSource audioMusic;
     public Scrollbar scrollbarMusic;
     public Scrollbar scrollbarEffects;
     public FadeManager fadeManager;
     private LineRenderer lineRenderer;
-
-
+    private FadeManager FadeManagerScrypt;
+    
     private void Start()
     {
+        FadeManagerScrypt = GameObject.Find("FadeManager").GetComponent<FadeManager>();
         audioMusic = GameObject.Find("Music/Audio Source").GetComponent<AudioSource>();
         
-        animator.SetBool("startTheGame", false);
-        animator.SetBool("fromMenuToOptions", false);
-        animator.SetBool("fromOptionsToMenu", false);
-        animator.SetBool("fromOptionsToSoundVolume", false);
-        animator.SetBool("fromSoundVolumeToOptions", false);
         scrollbarEffects.value = 1.0f;
         scrollbarMusic.value = 0.3f;  // The range is between 0.0-1.0  , i set up the volume to 0.3 since 1.0 is too loud.
-
-        //  used to draw the ray in the game 
-        lineRenderer = GetComponent<LineRenderer>();
-        lineRenderer.enabled = true;
-        lineRenderer.useWorldSpace = true;
     }
 
 
     public void Update()
     {
-        //  used for raycasting 
-        RaycastHit hit;
-        //  create a ray from the camera to where the mouse points to , should be replaced with the controller??
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-        //  make the ray visible in debug mode in the scene, need to find something else to make the ray visible in the game 
-        Debug.DrawRay(ray.origin, ray.direction * 1000000.0f , Color.red);
-        //  make the ray visible in game mode
-        lineRenderer.SetPosition(0, ray.origin + new Vector3(1.0f,0.0f,0.0f));
-        lineRenderer.SetPosition(1, ray.origin+ ray.direction * 1000000.0f);
+        if(Input.GetKeyDown(KeyCode.X)) // start the game 
+            {
+            startTheGameButtonIsPressed();
+            }
 
-
-
-        if(Physics.Raycast(ray, out hit))
+        if (Input.GetKeyDown(KeyCode.Z))  
         {
-
-            if (Input.GetMouseButtonDown(0))   //check if we have collision only if the maouse is clicked for eficiency // should br replaced with the controller button.
-            {
-                //  if we have an intersection 
-                if (hit.collider.CompareTag("Start"))
-                {
-                    startTheGameButtonIsPressed();
-                }
-
-                if (hit.collider.CompareTag("Options"))
-//<<<<<<< HEAD
-                {
-                    optionsButtonIsPressed();
-//=======
-			{
-				Debug.Log("Hit Option");
-                  //  optionsButtonIsPressed();
-//>>>>>>> d753a227c739e2a23c964303fbbf0cc979711d8c
-                }
-
-                if (hit.collider.CompareTag("Quit"))
-                {
-                    quitTheGameButtonIsPressed();
-                }
-
-                if (hit.collider.CompareTag("Back"))
-                {
-                    //backToMainMenuButtonIsPressed();
-                }
-
-                if (hit.collider.CompareTag("SoundOptions"))
-                {
-                  //  soundVolumeButtonIsPressed();
-                }
-
-                if (hit.collider.CompareTag("BackToOptions"))
-                {
-                   // backToOptionsButtonIsPressed();
-                }
-
-                if (hit.collider.CompareTag("MusicVolume")) // increase the sound volume
-                {
-                    scrollbarMusic.value += 0.1f;
-                }
-
-                if (hit.collider.CompareTag("SoundEffects")) // increase the sound Effects Volume
-                {
-                    scrollbarEffects.value += 0.1f;
-                }
-            }
-            // decrease the sound 
-            if (Input.GetMouseButtonDown(1))
-            {
-                if (hit.collider.CompareTag("MusicVolume")) // increase the sound volume
-                {
-                    scrollbarMusic.value -= 0.1f;
-                }
-
-                if (hit.collider.CompareTag("SoundEffects")) // increase the sound Effects Volume
-                {
-                    scrollbarEffects.value -= 0.1f;
-                }
-            }
+            quitTheGameButtonIsPressed();
         }
-        scrollbarSoundEffectsVolume();
-        scrollbarMusicVolume();
-    }
+
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            scrollbarMusic.value += 0.1f;
+            scrollbarMusicVolume();
+        }
+
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            scrollbarMusic.value -= 0.1f;
+            scrollbarMusicVolume();
+        }
+
 	}
-
-
-
 
     public void startTheGameButtonIsPressed()
     {
-        fadeManager.isTheCameraMoving = true;
-        audioEffects.Play();
-        animator.SetBool("startTheGame", true);
+       
+        FadeManagerScrypt.SetFadeInTrue();
         //Debug.Log("The Scene loaded successfully");
     }
-
 
     private void scrollbarMusicVolume() // this changes the volume of the music 
     {
@@ -138,58 +65,12 @@ public class MainMenu : MonoBehaviour
 
     private void scrollbarSoundEffectsVolume()
     {
-        audioEffects.volume = scrollbarEffects.value;
-    }
-
-
-
-
-
-    public void optionsButtonIsPressed()
-    {
-        animator.SetBool("startTheGame", false); // not necessary, just to be sure.
-
-
-        animator.SetBool("fromMenuToOptions", true);
-        animator.SetBool("fromOptionsToMenu", false);
-        animator.SetBool("fromOptionsToSoundVolume", false);
-        animator.SetBool("fromSoundVolumeToOptions", false);
-        audioEffects.Play();
-    }
-
-    public void backToMainMenuButtonIsPressed()
-    {
-        animator.SetBool("startTheGame", false); // not necessary, just to be sure.
-        animator.SetBool("fromMenuToOptions", false);
-        animator.SetBool("fromOptionsToMenu", true);
-        animator.SetBool("fromOptionsToSoundVolume", false);
-        animator.SetBool("fromSoundVolumeToOptions", false);
-        audioEffects.Play();
-    }
-
-    public void soundVolumeButtonIsPressed()
-    {
-        animator.SetBool("startTheGame", false); // not necessary, just to be sure.
-        animator.SetBool("fromMenuToOptions", false);
-        animator.SetBool("fromOptionsToMenu", false);
-        animator.SetBool("fromOptionsToSoundVolume", true);
-        animator.SetBool("fromSoundVolumeToOptions", false);
-        audioEffects.Play();
-    }
-
-    public void backToOptionsButtonIsPressed() // from the Volume Options
-    {
-        animator.SetBool("startTheGame", false); // not necessary, just to be sure.
-        animator.SetBool("fromMenuToOptions", false);
-        animator.SetBool("fromOptionsToMenu", false);
-        animator.SetBool("fromOptionsToSoundVolume", false);
-        animator.SetBool("fromSoundVolumeToOptions", true);
-        audioEffects.Play();
+        //audioEffects.volume = scrollbarEffects.value;
     }
 
     public void quitTheGameButtonIsPressed()
     {
-        audioEffects.Play();
+  
         Application.Quit();
         Debug.Log("The game did exit successfully");
     }
