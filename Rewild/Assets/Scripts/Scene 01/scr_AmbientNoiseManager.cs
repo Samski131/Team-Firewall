1cb6e0ce4d7813e0cdb3345aeacc9bbc0aedb87d
Adam Campbell <acampbell97@hotmail.co.uk>
2018-04-19 17:37:15 +0100

    Added FMOD
    Added FMOD into the project and set it up. Can play some sound effect and has all the other game features working a intended.
    
    Having issues triggering the current sounds and may have issues working around the delay of the footsteps, randomising the scary sounds and queueing up random sounds via code

---
+++ Rewild/Assets/Scripts/Scene 01/scr_AmbientNoiseManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FMODUnity
+{
+    public class scr_AmbientNoiseManager : MonoBehaviour
+    {
+
+
+        public GameObject AudioSourceLeft;
+        public GameObject AudioSourceRight;
+        public GameObject AudioSourceBehind;
+
+        private StudioEventEmitter sourceLeft;
+        private StudioEventEmitter sourceRight;
+        private StudioEventEmitter sourceBehind;
+
+
+        // Use this for initialization
+        void Start()
+        {
+
+            sourceLeft = AudioSourceLeft.GetComponent<StudioEventEmitter>();
+            sourceRight = AudioSourceRight.GetComponent<StudioEventEmitter>();
+            sourceBehind = AudioSourceBehind.GetComponent<StudioEventEmitter>();
+            
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+
+
+}
